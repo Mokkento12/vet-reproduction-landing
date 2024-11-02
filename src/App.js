d022572283f720c046879af2c0b7668d7d7cfdde
roman alekseev <roma.alekseev.90@bk.ru>
2024-11-02 08:37:48 +0300

    Добавил слайдер с изображениями, изменил цвет заголовка на белый и убрал фон

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import AboutSection from "./components/AboutSection";
 import ServicesSection from "./components/ServicesSection";
 import ContactSection from "./components/ContactSection";
+import ImageSlider from "./components/ImageSlider";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ImageSlider />
       <AboutSection />
       <ServicesSection />
       <ContactSection />
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,51 @@
+// src/components/ImageSlider.js
+import React from "react";
+import Slider from "react-slick";
+
+const ImageSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+  };
+
+  const slidesData = [
+    {
+      id: 1,
+      title: "Профессиональная помощь в репродукции",
+      image: `${process.env.PUBLIC_URL}/images/pets-1.jpg`,
+    },
+    {
+      id: 2,
+      title: "Новейшие методы диагностики",
+      image: `${process.env.PUBLIC_URL}/images/pets-2.jpg`,
+    },
+    {
+      id: 3,
+      title: "Безопасность и забота",
+      image: `${process.env.PUBLIC_URL}/images/pets-3.jpg`,
+    },
+  ];
+
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        {slidesData.map((slide) => (
+          <div key={slide.id} className="slide">
+            <img src={slide.image} alt={slide.title} />
+            <div className="slide-text">
+              <h2>{slide.title}</h2>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default ImageSlider;
